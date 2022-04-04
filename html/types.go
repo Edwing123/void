b73962edb7ddd53bd1a15f53ea5eb24b101c541a
Edwing123 <egarciavalle2014@gmail.com>
2022-04-03 19:22:50 -0600

    chore: remove the type definition for Children

--- html/types.go
+++ html/types.go
@@ -4,10 +4,8 @@ type TrustedString string
 
 type Attributes map[string]string
 
-type Children []any
-
 type Node struct {
 	Element    string
 	Attributes Attributes
-	Children   Children
+	Children   []any
 }
